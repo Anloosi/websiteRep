4ca632867ff371464f0ccab03d5e965989394667
Prince.Kweku <Kwekuasiedu2020@gmail.com>
2023-06-16 22:43:23 +0000

    first commit

---
+++ website/README.md
@@ -0,0 +1,8 @@
+<h1>ReactJS Homepage</h1>
+
+<p>Features:</p>
+
+ReactJS
+React Router Dom
+React Hooks (useState)
+Props
---
+++ website/src/App.js
@@ -0,0 +1,11 @@
+import PagesRouter from "./routes/PagesRouter";
+
+function App() {
+    return (
+        <div>
+            <PagesRouter favoriteFood="hotdogs" />
+        </div>
+    );
+}
+
+export default App;
